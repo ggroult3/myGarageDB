cc63eb5c766d2a118aa1bebb88fa18fbbc3f8178
Guillaume Groult <guillaume.groult3@orange.fr>
2020-11-20 01:14:55 +0100

    Création du serveur node.js
    Serveur crée avec Express permettant d'afficher une table MySQL dans un document HTML via un template EJS. Pensez à faire npm install avant de lancer le serveur en local.

--- server.js
+++ server.js
@@ -1,29 +1,38 @@
-var mysql = require('mysql');
-var db = mysql.createConnection({
+const express = require('express') // Middleware Express utilisé pour créer le serveur (gestion des différentes routes)
+const app = express() // Création du serveur app
+const mysql = require('mysql') // Module mysql permettant la connexion de notre webapp au serveur de la base de données
+const ejs = require('ejs') // Module permettant le rendu HTML généré de manière dynamique (Embedded JavaScript)
 
-   host: "localhost",
+const PORT = process.env.PORT || 8080 // Définition du port sur lequel le serveur sera écouté
 
-   user: "admini", // techni
+var result = [] // Contient les résultats de la requête SQL demandé
 
-   password: "passadm" // passtech
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "techni", // admini ou techni
+  password: "passtech" // passadm ou passtech
+}); // Créée une connexion avec la base de données
 
- });
- 
-  db.connect(function(err) {
+app.use('/',express.static(__dirname + '/assets')) // Permet l'utilisation des fichiers présents de le dossier /assets
 
-   if (err) throw err;
+app.get('/',function(req,res){
+  res.status(200).render(__dirname + '/assets/index.ejs',{result:result}) // Fait le rendu de l'index.ejs lors d'une requête GET /
+}) 
 
-   console.log("Connecté à la base de données MySQL!");
-   
-   db.query("SELECT * FROM projet.administrateur", function (err, result) {
+app.post('/mysql',function(req,res){ // Requête POST du formulaire /mysql
+  res.status(200)
+  console.log('Requête SQL envoyée !')
+  connection.query("SELECT * FROM projet.administrateur", function (err, data) { // Effectue une requête SQL
+    if (err) throw err;
+    result = data // Stocke les résultats de la requête SQL pour le rendu de index.ejs
+    console.log(result);
+  });
+  res.redirect('/') // Fait une redirection à l'adresse principale
+}) 
 
-       if (err) throw err;
 
-       console.log(result);
-
-     });
- 
-	 
-	
-
- });
+app.listen(PORT,() => { // Démarre le serveur en local sur le port PORT
+  console.log('\nServeur démarré !')
+  console.log('Ouvrez un navigateur et allez l\'adresse suivante :  localhost:' + PORT)
+  console.log('Pour arrêter le serveur, faites CTRL+C dans son terminal/invite de commande')
+}) 
